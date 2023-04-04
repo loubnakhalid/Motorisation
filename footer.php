62497e99cf191c77f3987841cfa9e0bc40d255b4
loubna <loubnakhalid09@gmail.com>
2023-04-04 13:57:05 +0000

    update

--- footer.php
+++ footer.php
@@ -390,7 +390,7 @@ if(isset($_GET['Guide'])){
                         </section>";
     }
 }
-if(isset($_GET['QSN'])){
+elseif(isset($_GET['QSN'])){
     echo "
         <section id='Qui'>
     <div class='NousSommes'>
@@ -452,7 +452,7 @@ if(isset($_GET['QSN'])){
     </section>
     ";
 }
-if(isset($_GET['Liv'])){
+elseif(isset($_GET['Liv'])){
         echo"<section id='livraison'>
         <div classe='prt1'>
         <img src='inc/img/livraison.png' class='livrimg1'><img >
@@ -552,6 +552,68 @@ if(isset($_GET['Liv'])){
             </table>
         </div>
     </section>";
+}
+elseif(isset($_GET['Grts'])){
+  ?>
+  <section id='garanties'>
+<div classe='prt1'>
+     <img src='inc/img/Garanties.png' class='Garantimg1'><img >
+  </div>
+    <div>
+        <h4 class='Stitle1'>Description des garanties offertes :</h4>
+    </div>
+    <p class='GarantPara'> 
+    Nous sommes fiers de proposer des garanties de qualité pour tous nos produits 
+    de motorisation. Nous avons confiance en la qualité et la durabilité de nos produits
+    et nous voulons que nos clients soient entièrement satisfaits de leur achat.Nous offrons
+    une garantie standard de 2 ans sur tous nos produits. Cette garantie couvre tous les 
+    défauts de fabrication et de matériaux. Si votre produit tombe en panne pendant la période
+    de garantie, nous le réparerons gratuitement ou le remplacerons par un produit neuf.<br>
+
+    Nous proposons également une garantie de satisfaction de 30 jours. Si vous n'êtes pas
+    entièrement satisfait de votre achat, vous pouvez le retourner pour un remboursement 
+    complet ou un échange. Cette garantie s'applique à tous les produits, à l'exception des
+    produits personnalisés.<br>
+
+    Enfin, nous offrons une garantie de durabilité pour nos produits de motorisation. 
+    Nous sommes convaincus que nos produits sont conçus pour durer, c'est pourquoi nous
+    <br>
+    </p>
+    <div>
+        <h4 class='Stitle1'>Procédure de réclamation :</h4>
+    </div>
+    <p class='GarantPara'>
+    Si vous souhaitez soumettre une réclamation pour votre moteur, veuillez suivre
+    les étapes ci-dessous :<br>
+
+    <span style='color: orange ; font-size:17px ;font-weight:600;margin-left:15px;margin-right:5px'>1 _</span>
+    Vérifiez la garantie de votre moteur pour vous assurer que votre réclamation est couverte. Vous pouvez trouver
+    les informations de garantie sur la page 'Garanties' de notre site web.<br>
+
+    <span style='color: orange ; font-size:17px ;font-weight:600;margin-left:15px;margin-right:5px'>2 _</span>
+    Contactez notre service clientèle par téléphone ou par e-mail pour obtenir un numéro de 
+    dossier de réclamation. Vous devrez fournir les informations suivantes : le numéro de série
+    de votre moteur, la date d'achat et la description détaillée du problème que vous 
+    rencontrez.<br>
+
+    <span style='color: orange ; font-size:17px ;font-weight:600;margin-left:15px;margin-right:5px'>3 _</span>
+    Envoyez votre moteur à notre centre de service agréé pour qu'il soit inspecté et évalué.
+    Le moteur doit être expédié dans son emballage d'origine ou dans un emballage approprié 
+    pour éviter tout dommage pendant le transport. Assurez-vous d'inclure le numéro de dossier
+    de réclamation avec votre moteur.<br>
+
+    <span style='color: orange ; font-size:17px ;font-weight:600;margin-left:15px;margin-right:5px'>4 _</span>
+    Notre équipe de service clientèle examinera les résultats de l'inspection et vous 
+    contactera pour vous informer de la décision prise concernant votre réclamation. 
+    Si votre réclamation est approuvée, nous procéderons à la réparation ou au remplacement 
+    de votre moteur conformément à nos conditions de garantie.<br>
+
+    Nous sommes là pour vous aider à résoudre ce problème et nous ferons tout notre possible
+     pour vous assister dans le processus de réclamation. Si vous avez des questions ou 
+     des préoccupations, n'hésitez pas à nous contacter.<br>
+    </p>
+</section>
+  <?php
 }
 ?>
 <?php include('./inc/bas.inc.html'); ?>
